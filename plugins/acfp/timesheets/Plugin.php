66935e4bf46ad691fcf2eb71dd195506e8b8fa39
Luke Towers <git@luketowers.ca>
2024-07-26 03:20:00 -0600

    Initial documentation for project

---
+++ plugins/acfp/timesheets/Plugin.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace ACFP\Timesheets;
+
+use Backend;
+use Backend\Models\UserRole;
+use System\Classes\PluginBase;
+
+/**
+ * Timesheets Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * Returns information about this plugin.
+     */
+    public function pluginDetails(): array
+    {
+        return [
+            'name'        => 'acfp.timesheets::lang.plugin.name',
+            'description' => 'acfp.timesheets::lang.plugin.description',
+            'author'      => 'ACFP',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+    /**
+     * Register method, called when the plugin is first registered.
+     */
+    public function register(): void
+    {
+
+    }
+
+    /**
+     * Boot method, called right before the request route.
+     */
+    public function boot(): void
+    {
+
+    }
+
+    /**
+     * Registers any frontend components implemented in this plugin.
+     */
+    public function registerComponents(): array
+    {
+        return []; // Remove this line to activate
+
+        return [
+            \ACFP\Timesheets\Components\MyComponent::class => 'myComponent',
+        ];
+    }
+
+    /**
+     * Registers any backend permissions used by this plugin.
+     */
+    public function registerPermissions(): array
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'acfp.timesheets.some_permission' => [
+                'tab' => 'acfp.timesheets::lang.plugin.name',
+                'label' => 'acfp.timesheets::lang.permissions.some_permission',
+                'roles' => [UserRole::CODE_DEVELOPER, UserRole::CODE_PUBLISHER],
+            ],
+        ];
+    }
+
+    /**
+     * Registers backend navigation items for this plugin.
+     */
+    public function registerNavigation(): array
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'timesheets' => [
+                'label'       => 'acfp.timesheets::lang.plugin.name',
+                'url'         => Backend::url('acfp/timesheets/mycontroller'),
+                'icon'        => 'icon-leaf',
+                'permissions' => ['acfp.timesheets.*'],
+                'order'       => 500,
+            ],
+        ];
+    }
+}
---
+++ plugins/acfp/timesheets/lang/en/lang.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'plugin' => [
+        'name' => 'Timesheets',
+        'description' => 'No description provided yet...',
+    ],
+    'permissions' => [
+        'some_permission' => 'Some permission',
+    ],
+];
